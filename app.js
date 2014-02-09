81ae7640c6992d6d67568d2c5fcc416296fcd94c
Josh Harris <joshharris11@gmail.com>
2014-02-09 14:45:42 -0600

    making request with simple polling

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ var express = require('express'),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
 
+var options = {
+    host: '10.194.142.32',
+    path: '/trimBatchWeb/dataCurrency',
+    //since we are listening on a custom port, we need to specify it by hand
+    port: '8080',
+    //This is what changes the request to a POST request
+    method: 'GET'
+};
+
+// Use promises you noob
+var callback = function(response) {
+    var str = ''
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+
+    response.on('end', function () {
+        console.log(str);
+    });
+}
+
+setInterval(function () {
+    var req = http.request(options, callback);
+    req.end();
+}, 2000);
+
+
+
 app.get('/', function(req, res) {
     res.send('<!doctype html> \
             <html> \
