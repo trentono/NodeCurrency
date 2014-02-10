63f52142bb1280207e8c22cd4082ace21f449630
David Erichsen <dave.erichsen@gmail.com>
2014-02-10 10:28:15 -0600

    removed old comments

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ angular.module('dataCurrency', ['socket-io']);
 
 angular.module('dataCurrency').controller('socketController', ['$scope', 'socket', function($scope, socket){
   $scope.trueCount = 0;
-  //var socket = io.connect();
+
   socket.on("message", function(msg){
       $scope.trueCount++;
-      //$scope.$apply();
   });
 }]);
